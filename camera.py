013519c271b7664ed800b3a6de63712a17ec1e40
Carlos Vieira <vieiramecarlos@gmail.com>
2019-03-20 19:02:02 -0300

    separate camera constants

--- camera.py
+++ camera.py
@@ -5,6 +5,15 @@ from track import draw_face, get_feature_boundbox, \
     get_inclination, apply_sprite, is_mouth_open
 
 class Camera(object):
+    CAPTURES_DIR = "static/captures/"
+    LANDMARKS_PATH = "shape_predictor_68_face_landmarks.dat"
+    EYE_SPRITES = {'googly':('googly_left.png', 'googly_right.png'),
+        'hearts':('heart.png', 'heart.png'), 
+        'CS':('CS.png', 'CS.png')}
+    EYE_SPRITES['crazy'] = EYE_SPRITES['googly']
+
+    RESIZE_RATIO = 1.0
+
     def __init__(self):
         self.video = cv.VideoCapture(0)
 
@@ -12,8 +21,7 @@ class Camera(object):
         self.face_detector = dlib.get_frontal_face_detector()
 
         # load facemark detector
-        landmarks_path = "shape_predictor_68_face_landmarks.dat"
-        self.facemark_detector = dlib.shape_predictor(landmarks_path)
+        self.facemark_detector = dlib.shape_predictor(Camera.LANDMARKS_PATH)
     
     def __del__(self):
         self.video.release()
@@ -23,7 +31,9 @@ class Camera(object):
         if not success:
             return
 
-        frame = cv.resize(frame, None, fx=0.8, fy=0.8)
+        if (Camera.RESIZE_RATIO != 1):
+            frame = cv.resize(frame, None, fx=Camera.RESIZE_RATIO, \
+                fy=Camera.RESIZE_RATIO)
         self.facemark(frame, mode)
 
         ret, jpeg = cv.imencode('.jpg', frame)
@@ -31,12 +41,15 @@ class Camera(object):
 
     def capture(self, mode):
         _, frame = self.video.read()
-        frame = cv.resize(frame, None, fx=0.8, fy=0.8)
+        if (Camera.RESIZE_RATIO != 1):
+            frame = cv.resize(frame, None, fx=Camera.RESIZE_RATIO, \
+                fy=Camera.RESIZE_RATIO)
         self.facemark(frame, mode)
         _, jpeg = cv.imencode('.jpg', frame)
         timestamp = strftime("%d-%m-%Y-%Hh%Mm%Ss", localtime())
-        filename = "static/captures/"+ timestamp +".jpg"
-        cv.imwrite(filename, frame)
+        filename = Camera.CAPTURES_DIR + timestamp +".jpg"
+        if not cv.imwrite(filename, frame):
+            raise RuntimeError("Unable to capture image "+timestamp)
         return timestamp
 
     def facemark(self, frame, mode):
@@ -51,16 +64,12 @@ class Camera(object):
 
             if mode == 'landmark':
                 draw_face(frame, shape)
-            if mode in ('googly', 'hearts', 'CS', 'crazy'):
+            if mode in Camera.EYE_SPRITES.keys():
                 leye = get_feature_boundbox(shape, 'leyebrow')
                 reye = get_feature_boundbox(shape, 'reyebrow')
 
-                eye_sprites = {'googly':('googly_left.png', 'googly_right.png'),
-                    'hearts':('heart.png', 'heart.png'), 
-                    'CS':('CS.png', 'CS.png')}
-                eye_sprites['crazy'] = eye_sprites['googly']
-                apply_sprite(frame, "sprites/"+eye_sprites[mode][0], leye, inclination)
-                apply_sprite(frame, "sprites/"+eye_sprites[mode][1], reye, inclination)
+                apply_sprite(frame, "sprites/"+Camera.EYE_SPRITES[mode][0], leye, inclination)
+                apply_sprite(frame, "sprites/"+Camera.EYE_SPRITES[mode][1], reye, inclination)
             if mode in ('rainbow', 'crazy') and is_mouth_open(shape, threshold=20):
                 mouth = get_feature_boundbox(shape, 'lips')
                 apply_sprite(frame, "sprites/rainbow.png", mouth, inclination)
